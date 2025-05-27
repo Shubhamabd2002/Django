20fb4c7c3bf13d5c98f2b811f01691129ad3d264
SK SHUBHAM <skshubham.abd@gmail.com>
2025-05-27 14:05:24 +0530

    addition of user_regTodo app for user registration

--- django_learning_todo_2/django_learning_todo_2/settings.py
+++ django_learning_todo_2/django_learning_todo_2/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'todo_app',
     'rest_framework',
+    'user_regTodo',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +51,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'django_learning_todo_2.urls'
 
