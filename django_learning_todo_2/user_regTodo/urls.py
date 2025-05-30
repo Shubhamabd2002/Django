c1919eed7ad7cd62a965dcf26bb196b21d5a7e36
SK SHUBHAM <skshubham.abd@gmail.com>
2025-05-30 17:42:56 +0530

    Integrated login and logout for todo

--- django_learning_todo_2/user_regTodo/urls.py
+++ django_learning_todo_2/user_regTodo/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('regester/', views.userRegestrition, name='user_regestritation')
+    path('regester/', views.userRegestrition, name='user_regestritation'),
+    path('login/', views.userLoginView, name='user_login'),
+    path('logout/', views.userLogoutView, name='user_logout'),
 ]
--- django_learning_todo_2/user_regTodo/views.py
+++ django_learning_todo_2/user_regTodo/views.py
@@ -5,6 +5,7 @@ import json
 from . import utils
 from django.views.decorators.csrf import csrf_exempt
 from .import serializers
+from django.contrib.auth import login, logout, authenticate
 
 @csrf_exempt
 def userRegestrition(request):
@@ -18,6 +19,44 @@ def userRegestrition(request):
             return JsonResponse({'message': 'User registration successfull', 'user': serializers.UserRegTodo(user)}, status = 201)
         except IntegrityError as e:
             return JsonResponse({"error": str(e)}, status=400)
+        except Exception as e:
+            return JsonResponse({"error": "An unexpected error occurred: " + str(e)}, status=400)
+    else:
+        return JsonResponse({"error": "Only POST requests are supported for this endpoint."}, status=405)
+
+@csrf_exempt
+def userLoginView(request):
+    if request.method == 'POST':
+        try:
+            logData = json.loads(request.body)
+            username = logData.get('username')
+            password = logData.get('password')
+
+            if not username or not password:
+                return JsonResponse({'error': 'Username and password are required.'}, status = 400)
+            user = authenticate(request, username = username, password = password)
+            if user is not None:
+                login(request, user)
+                return JsonResponse({'message': 'Login successful!'}, status = 200)
+            else:
+                return JsonResponse({"error": "Invalid credentials."}, status=401)
+        except IntegrityError as e:
+            return JsonResponse({"error": "Invalid JSON in request body." + str(e)}, status=400)
+        except Exception as e:
+            return JsonResponse({"error": "An unexpected error occurred: " + str(e)}, status=400)
+    else:
+        return JsonResponse({"error": "Only POST requests are supported for this endpoint."}, status=405)
+
+        
+@csrf_exempt
+def userLogoutView(request):
+    if request.method == 'POST':
+        try:
+            if request.user.is_authenticated:
+                logout(request)
+                return JsonResponse({"message": "Logout successful!"}, status=200)
+        except IntegrityError as e:
+            return JsonResponse({"error": "Invalid JSON in request body." + str(e)}, status=400)
         except Exception as e:
             return JsonResponse({"error": "An unexpected error occurred: " + str(e)}, status=400)
     else:
